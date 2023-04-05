600888c9ec73033e4e559f363b5c38aa53b3f6bb
Yevhen-Zhyvenkov <zheka123730@gmail.com>
2023-04-05 20:46:02 +0300

    Update 02-lightbox.js
    hw7

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -15,29 +15,19 @@ const createGalleryItem = ({ preview, original, description }) => `
 const galleryMarkup = galleryItems.map(createGalleryItem).join('');
 galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
 
-galleryList.addEventListener('click', onClick);
-
-function onClick(evt) {
-  evt.preventDefault();
-  if (!evt.target.classList.contains('gallery__image')) {
-    return;
-  }
-const galleryItem = evt.target.closest('.gallery__item');
-const originalUrl = galleryItem.querySelector('.gallery__image').dataset.source;
-    console.log(originalUrl)
 const gallery = new SimpleLightbox('.gallery a', {
         
   captionsData: 'alt',
   captionDelay: 250,
   captionPosition: 'bottom',
 });
-    console.log(gallery)
+    
 
 
 
     
     
-}
+
 
 
 
